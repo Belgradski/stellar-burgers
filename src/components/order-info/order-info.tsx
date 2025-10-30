7cd00b99d3bfe611bd70845c653b890412d67f5c
Alexey Belgradski <Alexeybelgradski@yandex.ru>
2025-10-30 19:42:32 +0300

    update OrderInfo

--- src/components/order-info/order-info.tsx
+++ src/components/order-info/order-info.tsx
@@ -1,21 +1,22 @@
-import { FC, useMemo } from 'react';
+import { FC, useEffect, useMemo } from 'react';
 import { Preloader } from '../ui/preloader';
 import { OrderInfoUI } from '../ui/order-info';
 import { TIngredient } from '@utils-types';
+import { useDispatch, useSelector } from '../../services/store';
+import { useParams } from 'react-router-dom';
+import { fetchOrder } from '../../services/slices/orders';
 
 export const OrderInfo: FC = () => {
+  const dispatch = useDispatch();
+  const { number } = useParams<{ number: string }>();
   /** TODO: взять переменные orderData и ingredients из стора */
-  const orderData = {
-    createdAt: '',
-    ingredients: [],
-    _id: '',
-    status: '',
-    name: '',
-    updatedAt: 'string',
-    number: 0
-  };
+  const { orderModalData: orderData } = useSelector((state) => state.orders);
 
-  const ingredients: TIngredient[] = [];
+  const { data: ingredients } = useSelector((state) => state.ingredients);
+
+  useEffect(() => {
+    dispatch(fetchOrder(Number(number)));
+  }, [dispatch]);
 
   /* Готовим данные для отображения */
   const orderInfo = useMemo(() => {
