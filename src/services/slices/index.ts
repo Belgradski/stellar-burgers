c65f5165cc5f94a031cfd3500a214bd963d20ec5
Alexey Belgradski <Alexeybelgradski@yandex.ru>
2025-11-24 15:05:47 +0300

    feature: добавлено тестирование builderReducer

---
+++ src/services/slices/index.ts
@@ -0,0 +1,31 @@
+export {
+  fetchIngredients,
+  initialState as ingredientsInitialState
+} from './ingredients';
+
+export { fetchFeeds, initialState as feedsInitialState } from './feeds';
+
+export {
+  fetchUser,
+  updateUser,
+  register,
+  login,
+  logout,
+  initialState as userInitialState
+} from './user';
+
+export {
+  setBun,
+  addIngredient,
+  removeIngredient,
+  resetConstructor,
+  initialState as constructorInitialState
+} from './builder';
+
+export {
+  fetchOrder,
+  fetchOrders,
+  createOrder,
+  resetOrderModalData,
+  initialState as ordersInitialState
+} from './orders';
